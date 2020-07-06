94b5fc9957127bb29a0729b1518a0905d46b3e97
Rupinder Kumar <rupinder.kumar@accionlabs.com>
2020-07-06 20:13:28 +0530

    state setup and reusable component design

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
-
+import SquareScreen from "./src/screens/SquareScreen";
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Square: SquareScreen
   },
   {
     initialRouteName: "Home",
